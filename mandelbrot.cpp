c5af8630910625ac68dfa05c29feb7290523b019
Nigel Damian <ndamian4@unm.edu>
2019-11-22 17:15:35 -0700

    New Colors

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -8,50 +8,43 @@ extern "C"{
 
 int main()
 {
-gfx_open( 800, 800, "mandelbrot");
-//gfx_clear_color(51,51,51);
-//gfx_clear();
-//gfx_point (200,200);
-//gfx_point (150,150);
-//gfx_point (100,100);
-for (int x=0; x<800;x++){
-       for(int y=0; y<800; y++)
-       {
-	       int n = 0;
-	       int z = 0;
-	       double a = 0;
-	       double b = 0;
-	       a = ((x - 400) * .005);
-               b = ((y - 400) * .005);
+	gfx_open( 800, 800, "mandelbrot");
 
-	 //      gfx_point(x,y);
-	       double ca = a;
-	       double cb = b;
-
-	       while (n < 100)
+	for (int x=0; x<800;x++)
+	{
+	       for(int y=0; y<800; y++)
 	       {
-		       double aa = (a*a) - (b*b);
-		       double bb = 2*a*b;
-		       a = aa + ca;
-		       b = bb + cb;
-		       if (abs(a + b) > 16)
+		       int n = 0;
+		       int z = 0;
+		       double a = 0;
+		       double b = 0;
+		       a = ((x - 400) * .005);
+	               b = ((y - 400) * .005);
+		       double ca = a;
+		       double cb = b;
+	
+		       while (n < 100)
 		       {
-			       break;
+			        double aa = (a*a) - (b*b);
+			        double bb = 2*a*b;
+			        a = aa + ca;
+			        b = bb + cb;
+				if (abs(a+b) > 16.0)
+				{
+				       break;
+		       		}
+				n++;
 		       }
-		       n++;
-	       }
-	       double color = ((n * (3/n)) % 255);
-	       double color1 = ((n * n ) % 255);    // change color
-	       double color2 = ((n / 1.5) % 255);
-	       gfx_color(color,color1,color2);
-	       if (n == 100)
-	       {
-		       gfx_point(x,y);
+		       
+	        	double color = n*n % 250;
+	        	double color1 = (n * 3/2) % 250;    // change color
+	        	double color2 = n % 250;
+			gfx_color(color, color1, color2);
+			gfx_point(x,y);
 	       }
        }
-}       
-int button = gfx_wait();
-return 0;
+	int button = gfx_wait();
+	return 0;
 }
 
 
