eca34f3360877fcf0c62f6a3697ec080c667ee20
Nigel Damian <ndamian4@unm.edu>
2019-11-22 17:30:38 -0700

    :tree: Changed Var names

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -12,16 +12,16 @@ int main()
 {
 	gfx_open( WINDOW_WIDTH, WINDOW_HEIGHT, "Mandelbrot");
 
-	for (int x = 0; x < WINDOW_WIDTH; x ++)
+	for (int xPos = 0; xPos < WINDOW_WIDTH; xPos ++)
 	{
-	       for(int y = 0; y < WINDOW_HEIGHT; y++)
+	       for(int yPos = 0; yPos < WINDOW_HEIGHT; yPos ++)
 	       {
 		       int n = 0;
 		       int z = 0;
 		       double a = 0;
 		       double b = 0;
-		       a = ((x - 400) * .005);
-	               b = ((y - 400) * .005);
+		       a = ((xPos - 400) * .005);
+	               b = ((yPos - 400) * .005);
 		       double ca = a;
 		       double cb = b;
 	
@@ -37,12 +37,12 @@ int main()
 		       		}
 				n++;
 		       }
-		       
+
 	        	double red = n*n*n % 255;
 	        	double green = (n * 3/2) % 256;    // change color
 	        	double blue = n / 2 % 255;
 			gfx_color(red, green, blue);
-			gfx_point(x,y);
+			gfx_point(xPos,yPos);
 	       }
         }
 	int button = gfx_wait();
